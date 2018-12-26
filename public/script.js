5b3c3d6aa0ca1fda1ad2b970cb071931be5acccd
user <user@lab1600m.tumo.lab>
2018-12-26 19:08:19 +0400

    sketch

--- public/script.js
+++ public/script.js
@@ -1,27 +1,25 @@
 let socket;
-let randomColor;
+let color;
 function setup() {
+    color = '#f6a6df';
     noStroke()
     createCanvas(1000, 1000);
     background('grey');
     socket = io.connect('https://arkhsketchio.herokuapp.com/')
-
 }
 function draw() {
-    randomColor = floor(random(255));
     socket.on('mouse', data => {
         rectMode(CENTER);
-        fill('#f6a6df;');
+        fill(color);
         ellipse(data.x, data.y, 40, 40);
-    })
+    });
 }
 function mouseDragged() {
-    randomColor = random(255);
-    fill(0, randomColor, 0)
+    fill('grey');
     ellipse(mouseX, mouseY, 50, 50);
     socket.emit('mouse', {
         x: mouseX,
         y: mouseY,
-        randomColor: randomColor
+        color:color
     })
 }
