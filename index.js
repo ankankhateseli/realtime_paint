1418c8090605f4a9d9eb3db2074028185416e988
lofigrammer <lofigrammer@gmail.com>
2022-12-03 03:59:58 +0400

    Format Code

--- index.js
+++ index.js
@@ -1,12 +1,12 @@
-const express = require('express');
+const express = require("express");
 const app = express();
-const port = process.env.PORT  ||3000;
+const port = process.env.PORT || 3000;
 const server = app.listen(port);
-app.use(express.static('public'))
-const socket = require('socket.io')
+app.use(express.static("public"));
+const socket = require("socket.io");
 const io = socket(server);
-io.sockets.on('connection', socket => {
-    socket.on('mouse', data => {
-        socket.broadcast.emit('mouse', data)
-    })
-})
+io.sockets.on("connection", (socket) => {
+  socket.on("mouse", (data) => {
+    socket.broadcast.emit("mouse", data);
+  });
+});
--- public/script.js
+++ public/script.js
@@ -1,25 +1,25 @@
 let socket;
 let color;
 function setup() {
-    color = '#f6a6df';
-    noStroke()
-    createCanvas(1000, 1000);
-    background('grey');
-    socket = io.connect('https://arkhsketchio.herokuapp.com/')
+  color = "#f6a6df";
+  noStroke();
+  createCanvas(1000, 1000);
+  background("grey");
+  socket = io.connect("https://arkhsketchio.herokuapp.com/");
 }
 function draw() {
-    socket.on('mouse', data => {
-        rectMode(CENTER);
-        fill(color);
-        ellipse(data.x, data.y, 40, 40);
-    });
+  socket.on("mouse", (data) => {
+    rectMode(CENTER);
+    fill(color);
+    ellipse(data.x, data.y, 40, 40);
+  });
 }
 function mouseDragged() {
-    fill('black');
-    ellipse(mouseX, mouseY, 50, 50);
-    socket.emit('mouse', {
-        x: mouseX,
-        y: mouseY,
-        color:color
-    })
+  fill("black");
+  ellipse(mouseX, mouseY, 50, 50);
+  socket.emit("mouse", {
+    x: mouseX,
+    y: mouseY,
+    color: color,
+  });
 }
